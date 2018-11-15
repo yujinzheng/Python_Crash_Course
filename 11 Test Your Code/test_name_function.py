5f6378ca305dd9f63271fe36db6d6ab44dfa343a
yujinzheng <632463098@qq.com>
2018-11-16 02:02:55 +0800

    修改人：于金正 修改时间：2018.11.16 02:02 修改理由：第一次添加内容，添加了一个测试的练习和ReadMe文件

---
+++ 11 Test Your Code/test_name_function.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+import unittest
+from name_function import get_formatted_name
+
+class NameTestCase(unittest.TestCase):
+    """测试name_function.py"""
+
+    def test_first_last_name(self):
+        """能够正确地处理象Janis Joplin这样的姓名吗？"""
+        formatted_name = get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+unittest.main()
---
+++ READEME.md
@@ -0,0 +1,4 @@
+# Read Me
+This is a project to record my learn road in Python Crash Course. I hope I can finish it.
+
+这是一个用来记录我在《Python编程从入门到实践》的学习之路。希望这次我能够完成它。
