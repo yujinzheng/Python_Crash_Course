083545b4651ac1bf37850c093cb8ba84d2478158
yujinzheng <632463098@qq.com>
2018-11-19 01:01:30 +0800

    练习添加了中间名的函数和对应的单元测试

--- 11 Test Your Code/test_name_function.py
+++ 11 Test Your Code/test_name_function.py
@@ -7,8 +7,13 @@ class NameTestCase(unittest.TestCase):
     """测试name_function.py"""
 
     def test_first_last_name(self):
-        """能够正确地处理象Janis Joplin这样的姓名吗？"""
+        """能够正确地处理像Janis Joplin这样的姓名吗？"""
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_last_middle_name(self):
+        """能够正确处理像Wolfgang Amadeus Mozart这样的姓名吗？"""
+        formatted_name = get_formatted_name('Wolfgang', 'Mozart', 'Amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
 
 unittest.main()
