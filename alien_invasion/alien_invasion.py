c88a74ac4626f5c7052f630d4de1a7811cd29a33
yujinzheng <632463098@qq.com>
2019-01-10 02:54:30 +0800

    项目进度： 1、刷新项目描述文件 2、在之前的基础上完成向左发射子弹的练习，此练习有重构空间，后续可以考虑重构 3、在左上角绘制出第一个外星人

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ import game_function as gf
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 from pygame.sprite import Group
 
 
@@ -17,6 +18,8 @@ def run_game():
 
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
+    # 创建外星人
+    alien = Alien(ai_settings, screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
 
@@ -24,8 +27,7 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 run_game()
