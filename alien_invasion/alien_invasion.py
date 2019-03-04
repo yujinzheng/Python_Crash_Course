fa23bac3e7b8062580570d24b6af4246ae162f98
yujinzheng <632463098@qq.com>
2019-03-05 01:36:29 +0800

    项目进展： 1、成功绘制Play按钮，实现点击Play按钮重新开始游戏的操作 2、完成练习14-1，实现按P开始游戏的功能

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -7,6 +7,7 @@ from settings import Settings
 from ship import Ship
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
 
 
 def run_game():
@@ -27,14 +28,17 @@ def run_game():
     # 创建一个用来存储游戏统计信息的实例
     stats = GameStats(ai_settings)
 
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
     # 开始游戏的主循环
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game()
